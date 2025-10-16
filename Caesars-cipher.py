daf97ef292d7f0212875750f67a36c0032a1f5f7
artembobrikov433-rgb <artembobrikov433@gmail.com>
2025-10-16 14:16:38 +0300

    Added a feature for selecting encryption

--- Caesars-cipher.py
+++ Caesars-cipher.py
@@ -1,3 +1,11 @@
 def get_folder_path():
     path = input("Enter the path to the folder:")
     return path
+
+def choose_action():
+    while True:
+        action = input("Select action (1 - encrypt, 2 - decrypt):")
+        if action in ("1", "2"):
+            return action
+        else:
+            print("Enter 1 or 2.")
